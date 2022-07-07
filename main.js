8851dd62f331e7eab0c91c49b03b64ab1212b832
avibedi1768 <avibedi1234@gmail.com>
2022-07-07 16:54:36 +0530

    firebase fixed

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDLy3u7BxPWMpAxJqPawtCCcf5Oxx5w_h4",
-  authDomain: "todo-b2fc0.firebaseapp.com",
-  projectId: "todo-b2fc0",
-  storageBucket: "todo-b2fc0.appspot.com",
-  messagingSenderId: "1034880743632",
-  appId: "1:1034880743632:web:4d8b33d74274b19b1346d5",
-  measurementId: "G-VGGGMTGSCH"
-};
-
-// Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-	  firebase.analytics();
+var db = firebase.database();
 
 if (window.localStorage.getItem('id') == undefined) {
-	window.localStorage.setItem('id', 0);
+	window.localStorage.setItem('id', 1);
 }
+function sendStuff(){
+	let chore = document.getElementById("chore").value;
+	let desc = document.getElementById("desc" ).value;
 
+	if (desc == "" || chore == ""){
+		window.alert("Please enter some data for the todo");
+	}
+	else{
+		var id = window.localStorage.getItem('id');
+		console.log(id);
+		db.ref("component " + id).set({
+			chore : chore,
+			desc : desc
+		})
+		
+		db.ref("id").set({
+			id: parseInt(id) + 1
+		})
+		sendToLocal(chore, desc, id);
+		window.localStorage.setItem('id',parseInt(id) + 1);
+		document.getElementById("chore").value = "";
+		document.getElementById("desc").value = "";
+	}
+}
+function sendToLocal(chore, desc, id) {
+	window.localStorage.setItem("task" + id, chore);
+	window.localStorage.setItem("desc" + id, desc);
+	var Desc = window.localStorage.getItem("desc" + id);
+	var Chore = window.localStorage.getItem("task" + id);
+	compo(Desc, Chore, id);
+}	
+function loadwala(){
+	for(i=0; i<100; i++){
+		if(window.localStorage.getItem("desc" + i) == undefined){
+			continue;
+		}
+			var Desc = window.localStorage.getItem("desc" + i);
+			var Chore = window.localStorage.getItem("task" + i);
+			compo(Desc, Chore, i);
+		}
+}
 function compo(Desc, Chore, id){
 	if (Desc == "" || Chore == ""){
 		window.alert("Please enter some data for the todo");
@@ -41,43 +67,4 @@ function compo(Desc, Chore, id){
 		}
 		document.getElementById("components").appendChild(div);
 	}
-}
-function sendStuff(){
-	var db = firebase.database();
-	let chore = document.getElementById("chore").value;
-	let desc = document.getElementById("desc" ).value;
-
-	if (desc == "" || chore == ""){
-		window.alert("Please enter some data for the todo");
-	}
-	else{
-		var id = window.localStorage.getItem('id');
-		console.log(id);
-		db.ref("component " + id).set({
-			chore : chore,
-			desc : desc
-		})
-		
-		db.ref("id/").set({
-			id: parseInt(id) + 1
-		})
-		window.localStorage.setItem('id',parseInt(id) + 1);
-		sendToLocal(chore, desc, id);
-		compo(desc,chore,id);
-	}
-}
-function sendToLocal(chore, desc, id) {
-	
-	window.localStorage.setItem("task" + id, chore);
-	window.localStorage.setItem("desc" + id, desc);
-}	
-function loadwala(){
-	for(i=0; i<100; i++){
-		if(window.localStorage.getItem("desc" + i) == undefined){
-			continue;
-		}
-			var Desc = window.localStorage.getItem("desc" + i);
-			var Chore = window.localStorage.getItem("task" + i);
-			compo(Desc, Chore, i);
-		}
 }
